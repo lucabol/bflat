3e8bb24d1c1e572d5d9d7da7118b3481a45d4802
Luca Bolognese <lucabol@microsoft.com>
2024-02-01 18:40:37 +0100

    cleanup

--- src/zerolib/Sys/Buffer.cs
+++ src/zerolib/Sys/Buffer.cs
@@ -8,19 +8,15 @@ namespace Sys;
 public static partial class Buffers
 {
     public const int K     = 1024;
-    public const int K4    = K * 4;
     public const int K8    = K * 8;
     public const int K16   = K * 16;
-    public const int K32   = K * 32;
 
     public const int M     = K * K;
     public const int M8    = M * 8;
 
+    // Inline arrays cannot be bigger than 1Mb (or sometimes a combination of smaller ones don't compile either, ?!).
     [InlineArray(K)]
     public struct K_Buffer<T> { private T _element0; }
-
-    [InlineArray(K4)]
-    public struct K4_Buffer<T> { private T _element0; }
 
     [InlineArray(K8)]
     public struct K8_Buffer<T> { private T _element0; }
@@ -28,21 +24,8 @@ public static partial class Buffers
     [InlineArray(K16)]
     public struct K16_Buffer<T> { private T _element0; }
 
-    [InlineArray(K32)]
-    public struct K32_Buffer<T> { private T _element0; }
 
-    // As it happens, inline arrays cannot be bigger than 1 Megabyte. No sure why.
-    public unsafe struct M8_Buffer {
-        fixed byte _buf[M8];
-
-        public System.Span<byte> Span {
-        get {
-            fixed (byte* p = _buf)
-                return new System.Span<byte>(p, M8);
-        } }
-    }
-
-    // This is ergonomically nice, but it goes into the data segment, producing a huge binary.
+    // This is ergonomically nice, but, used as static var, it goes into the data segment, producing a huge binary.
     public unsafe struct HugeBuffer<T> where T: unmanaged
     {
         fixed byte _buf[M8];
@@ -61,16 +44,16 @@ public static partial class Buffers
             => MemoryMarshal.CreateSpan(ref Unsafe.As<byte,T>(ref buffer._buf[0]), M8 / sizeof(T)); 
         public int Length => M8 / sizeof(T);
     }
-    /*
-    // Mads please implement this
-    public unsafe struct M8_Buffer<T> where T: unmanaged {
-        public fixed T _buf[M8];
 
-        public System.Span<T> Span {
+    // Using HugeBuffer<byte> gives compiler errors, so I need to create a new type.
+    public unsafe struct M8_Buffer {
+        fixed byte _buf[M8];
+
+        public System.Span<byte> Span {
         get {
-            fixed (T* p = _buf)
-                return new System.Span<T>(p, M8);
+            fixed (byte* p = _buf)
+                return new System.Span<byte>(p, M8);
         } }
     }
-    */
+
 }
