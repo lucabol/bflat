b5d0ba8d26452c1d9399116f166bbe8a40926cf0
Luca Bolognese <lucabol@microsoft.com>
2024-02-13 11:18:30 +0100

    Resolving conflicts

--- src/zerolib/Perf.bat
+++ src/zerolib/Perf.bat
@@ -1,2 +1,1 @@
 hyperfine --warmup 5 "TPOP.exe kjbible.txt" "NoAlloc.exe kjbible.txt" "Arena.exe kjbible.txt" "Standard.exe kjbible.txt"
-dir *.exe
--- src/zerolib/Perf.sh
+++ src/zerolib/Perf.sh
@@ -1,2 +1,1 @@
 hyperfine --warmup 5 "TPOP kjbible.txt" "NoAlloc kjbible.txt" "Arena kjbible.txt" "Standard kjbible.txt"
-ls -l TPOP NoAlloc Arena Standard
