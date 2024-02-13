723fc81ab2ade409d82f077f3434529d65bc983e
Luca Bolognese <lucabol@microsoft.com>
2024-02-13 11:41:52 +0100

    Data for windows

--- src/zerolib/Perf.bat
+++ src/zerolib/Perf.bat
@@ -1,1 +1,2 @@
-hyperfine --warmup 5 "TPOP.exe kjbible.txt" "NoAlloc.exe kjbible.txt" "Arena.exe kjbible.txt" "Standard.exe kjbible.txt"
+hyperfine --warmup 5 --export-markdown windows.md "TPOP.exe kjbible.txt" "NoAlloc.exe kjbible.txt" "Arena.exe kjbible.txt" "Standard.exe kjbible.txt"
+dir *.exe
---
+++ src/zerolib/windows.md
@@ -0,0 +1,3 @@
+| Command | Mean [s] | Min [s] | Max [s] | Relative |
+|:---|---:|---:|---:|---:|
+| `TPOP.exe kjbible.txt` | 1.179 ± 0.057 | 1.101 | 1.296 | 1.00 |
